6b1204aa621a770c8eb80686485d0b84e9346e92
Valentin Yordanov <valentin_yordanov1997@yahoo.com>
2017-12-11 12:00:25 +0200

    uploading the files

---
+++ Movie Review Sentiment Analyzer/src/bg/uni/sofia/fmi/mjt/sentiment/MovieReviewSentimentAnalyzer.java
@@ -0,0 +1,55 @@
+package bg.uni.sofia.fmi.mjt.sentiment;
+
+import java.util.Collection;
+
+public class MovieReviewSentimentAnalyzer implements SentimentAnalyzer {
+
+	@Override
+	public double getReviewSentiment(String review) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getReviewSentimentAsName(String review) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public double getWordSentiment(String word) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Collection<String> getMostFrequentWords(int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<String> getMostPositiveWords(int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<String> getMostNegativeWords(int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSentimentDictionarySize() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isStopWord(String word) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
