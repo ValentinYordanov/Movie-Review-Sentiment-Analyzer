b4e125def18897103831fc5727c05aa626ed73a1
Valentin Yordanov <valentin_yordanov1997@yahoo.com>
2017-12-12 23:11:05 +0200

    1st two tests

--- Movie Review Sentiment Analyzer/test/MovieReviewSentimentAnalyzerTest.java
+++ Movie Review Sentiment Analyzer/test/MovieReviewSentimentAnalyzerTest.java
@@ -1,12 +1,25 @@
+import static org.junit.Assert.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import bg.uni.sofia.fmi.mjt.sentiment.*;
+
 class MovieReviewSentimentAnalyzerTest {
 
+	MovieReviewSentimentAnalyzer test = new MovieReviewSentimentAnalyzer("movieReviews2.txt", "stopwords.txt");
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void IsAStopWordtest() {
+		
+		assertTrue("Should be a stopword!", test.isStopWord("a"));
+		
 	}
-
+	
+	@Test
+	void IsNotAStopWordtest() {
+		
+		assertFalse("Should NOT be a stopword!", test.isStopWord("aa"));
+		
+	}
 }
